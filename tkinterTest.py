f5d296f3643df9ddd19d85cdc114143fb9879d17
Richard <>
2021-02-06 15:00:31 -0500

    start of GUI in tkinter

---
+++ tkinterTest.py
@@ -0,0 +1,17 @@
+import tkinter
+
+def main():
+    window = tkinter.Tk()
+
+    window.title("ZooML")
+    window.geometry("1440x720")
+    label = tkinter.Label(window, text = "Spy on your Students").pack()
+
+
+
+
+
+
+    window.mainloop()
+
+main()
